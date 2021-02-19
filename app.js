a92c0a2b1236f94b92d5f3884a3131f1292cac13
Julien <j.dechavanne@24s.com>
2021-02-19 15:10:53 +0100

    Add env file and env support

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
-
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
+const app = express();
+
 // MIDDLEWARES
-app.use(morgan('dev'));
+//console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config.env'
+});
 const app = require('./app');
 
 // SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+console.log(`Run node env on ${process.env.NODE_ENV} mode`);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
