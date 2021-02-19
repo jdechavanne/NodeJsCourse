0fc2531d7b57d7477bfcc89ee88485f8ae7eaf34
Julien <j.dechavanne@24s.com>
2021-02-19 15:58:08 +0100

    fixup! fixup! add eslint and prettier dependancies

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // MIDDLEWARES
 //console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const app = require('./app');
 
 // SERVER
 const port = process.env.PORT || 3000;
-
-// eslint-disable-next-line no-console
 console.log(
   `Run node env on ${process.env.NODE_ENV} mode`
 );
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
 });
